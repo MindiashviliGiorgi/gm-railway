2ffd8716079e4a5c868f92a5a67161c822799f25
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2023-10-31 00:33:20 +0400

    give styles

--- src/app/passengers-data-page/passengers-data-page.component.ts
+++ src/app/passengers-data-page/passengers-data-page.component.ts
@@ -7,16 +7,9 @@ import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./passengers-data-page.component.scss']
 })
 export class PassengersDataPageComponent implements OnInit {
-  // DOM all places Div
-  @ViewChild('placeDivsA', { static: true }) placeDivsA!: ElementRef;
-  @ViewChild('placeDivsB', { static: true }) placeDivsB!: ElementRef;
-  selectedDiv: HTMLDivElement | null = null;
 
   storedData: any = {};
   myForm: FormGroup; // The main form
-  // passengerForm: FormGroup;
-  redd: boolean = false;
-  choiceTrain: boolean = true;
 
   constructor(private formBuilder: FormBuilder) {
     const storedDataString = localStorage.getItem('formData');
@@ -35,7 +28,6 @@ export class PassengersDataPageComponent implements OnInit {
       email : [''],
       phoneNumber : [''],
       passengers: this.formBuilder.array([]),
-      trainClass: [''], 
     });
   }
 
@@ -48,8 +40,6 @@ export class PassengersDataPageComponent implements OnInit {
       this.createPassenger();
     }
   }
-  
-  
 
   addContact() {
     this.storedData.email = this.myForm.get('email')?.value;
@@ -60,56 +50,14 @@ export class PassengersDataPageComponent implements OnInit {
   generatePeople(people : number): number[] {
     return Array.from({ length: people }, (index, i) => i + 1);
   }
-  //   // create passengers Array as FormArray from myForm
-  //   const passengers = this.myForm.get('passengers') as FormArray;
-  //   // create newPassenger as a passForm.Value Object
-  //   const newPassenger = this.formBuilder.group(this.passForm.value);
-  //   // Add an 'id' property to the passenger object and set it to the current index
-  //   newPassenger.addControl('id', new FormControl(passengers.length));
-  //   // push that newPassenger object in passengers array
-  //   passengers.push(newPassenger);
 
-  //   localStorage.setItem('formData', JSON.stringify(this.storedData));
-
-  //   this.storedData = this.myForm.value;
-  // }
-
-  // classList add and remove to choiced Div
-  // toggleRed(event: Event, div: HTMLDivElement) {
-  //   if (this.selectedDiv) {
-  //     this.selectedDiv.classList.remove('get-red');
-  //   }
-  //   this.selectedDiv = div;
-  //   div.classList.add('get-red');
-
-  //   // Update the trainPlace property in the myForm with the value of the clicked div
-  //   this.passForm.get('trainPlace')?.setValue(div.textContent?.trim() || '');
-
-  //   localStorage.setItem('formData', JSON.stringify(this.storedData));
-
-  //   this.storedData = this.myForm.value;
-
-  //   console.log(this.storedData)
-  // }
-
-  // ngAfterViewInit() {
-  //   const divElementsA = this.placeDivsA.nativeElement.querySelectorAll('.place-a');
-  //   divElementsA.forEach((div: HTMLDivElement) => {
-  //     div.addEventListener('click', (event) => this.toggleRed(event, div));
-  //   });
-
-  //   const divElementsB = this.placeDivsB.nativeElement.querySelectorAll('.place-b');
-  //   divElementsB.forEach((div: HTMLDivElement) => {
-  //     div.addEventListener('click', (event) => this.toggleRed(event, div));
-  //   });
-  // 
-  
   createPassenger() {
     const passengers = this.myForm.get('passengers') as FormArray;
     const passengerForm = this.formBuilder.group({
       firstName: [''],
       lastName: [''],
       personalNumber: [''],
+      trainClass: [''],
       place: [''],
     });
     passengers.push(passengerForm);
@@ -120,15 +68,11 @@ export class PassengersDataPageComponent implements OnInit {
   }
 
   addPassengerInfo(index: number){
-    const passengers = this.myForm.get('passengers') as FormArray;
-    const passengerForm = passengers.at(index) as FormGroup;
+    localStorage.setItem('formData', JSON.stringify(this.storedData));
+    
+    this.storedData = this.myForm.value;
 
-    const firstName = passengerForm.get('firstName')?.value;
-    const lastName = passengerForm.get('lastName')?.value;
-
-    console.log(firstName, lastName)
-
-    console.log(this.myForm.value)
+    
   }
   
 }
